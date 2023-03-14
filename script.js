0cf792b8e5c38036f3f5107619025246647300ae
firdiler <127399081+firdiler@users.noreply.github.com>
2023-03-14 22:16:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector('.navbar')
-const navbar_btn = document.querySelector('.header__navbar--btn')
-
-
-navbar_btn.addEventListener('click' , ()=>{
-    if(navbar.className=='navbar')navbar.classList.add('active')
-    else navbar.classList.remove('active')
-})
